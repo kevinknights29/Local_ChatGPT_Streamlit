4881683d29806ef5a3f693c30e49691dbd9abbb2
Kevin Knights <kevinknights29@hotmail.com>
2023-10-21 17:55:12 -0500

    added annotatios and doc strings

--- src/streamlit/widgets.py
+++ src/streamlit/widgets.py
@@ -5,8 +5,16 @@ import streamlit as st
 from src.openai import check_openai_api_key
 
 
-def openai_key_validation():
-    # Get an OpenAI API Key before continuing
+def openai_key_validation() -> None:
+    """
+    Validates and sets the OpenAI API Key.
+
+    This function checks if the OpenAI API Key is valid and sets it in the session state.
+    It prompts the user to enter the API Key if it is not already provided in the secrets.
+
+    Returns:
+        None
+    """
     openai_api_key = ""
     if "api_key_is_valid" not in st.session_state:
         if "openai" in st.secrets:
@@ -16,11 +24,11 @@ def openai_key_validation():
                 )
             else:
                 st.warning(
-                    "The OpenAI API Key provisioned is a invalid... Check secrets!",
+                    "The OpenAI API Key provisioned is invalid... Check secrets!",
                 )
                 st.session_state["api_key_is_valid"] = False
         else:
-            st.warning("The OpenAI API Key provisioned is a invalid... Check secrets!")
+            st.warning("The OpenAI API Key provisioned is invalid... Check secrets!")
             st.session_state["api_key_is_valid"] = False
     if not st.session_state["api_key_is_valid"]:
         openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
@@ -29,7 +37,7 @@ def openai_key_validation():
             st.stop()
         st.session_state["api_key_is_valid"] = check_openai_api_key(openai_api_key)
     if not st.session_state["api_key_is_valid"]:
-        st.warning("The OpenAI API Key provisioned is a invalid!")
+        st.warning("The OpenAI API Key provisioned is invalid!")
         st.stop()
-    st.success("The OpenAI API Key provisioned is a valid!")
+    st.success("The OpenAI API Key provisioned is valid!")
     st.session_state["api_key_is_valid"] = True
