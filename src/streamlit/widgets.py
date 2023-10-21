3f4a811da586edbc7a51488a1b8f77bd8e94a64d
Kevin Knights <kevinknights29@hotmail.com>
2023-10-21 17:49:15 -0500

    added streamlit widgets submodule

---
+++ src/streamlit/widgets.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import streamlit as st
+
+from src.openai import check_openai_api_key
+
+
+def openai_key_validation():
+    # Get an OpenAI API Key before continuing
+    openai_api_key = ""
+    if "api_key_is_valid" not in st.session_state:
+        if "openai" in st.secrets:
+            if "api_key" in st.secrets.openai:
+                st.session_state["api_key_is_valid"] = check_openai_api_key(
+                    st.secrets.openai.api_key,
+                )
+            else:
+                st.warning(
+                    "The OpenAI API Key provisioned is a invalid... Check secrets!",
+                )
+                st.session_state["api_key_is_valid"] = False
+        else:
+            st.warning("The OpenAI API Key provisioned is a invalid... Check secrets!")
+            st.session_state["api_key_is_valid"] = False
+    if not st.session_state["api_key_is_valid"]:
+        openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+        if not openai_api_key:
+            st.info("Enter an OpenAI API Key to continue")
+            st.stop()
+        st.session_state["api_key_is_valid"] = check_openai_api_key(openai_api_key)
+    if not st.session_state["api_key_is_valid"]:
+        st.warning("The OpenAI API Key provisioned is a invalid!")
+        st.stop()
+    st.success("The OpenAI API Key provisioned is a valid!")
+    st.session_state["api_key_is_valid"] = True
